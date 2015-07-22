cf626e46b1bc9f76af716d6c4e51191debaf46bf
Hubert Huang <hubert77@gmail.com>
2015-07-22 14:22:19 -0700

    Create Ozone::Formatter module for string formatting ozone times

--- lib/ozone.rb
+++ lib/ozone.rb
@@ -3,17 +3,22 @@ require "ozone/version"
 require 'active_support/time'
 
 module Ozone
-  module_function
+  module Formatter
+    module_function
 
-  def call(time:, offset:, observes_dst: true, format: '%Y-%m-%d %H:%M')
-    timezone = ActiveSupport::TimeZone[offset/60]
-    time_with_zone = time.in_time_zone(timezone)
-    if time_with_zone.dst? && !observes_dst
-      offset -= 120
+    def call(time:, offset:, observes_dst: true, format: '%Y-%m-%d %H:%M')
       timezone = ActiveSupport::TimeZone[offset/60]
-      2.hours.since(time.in_time_zone(timezone)).strftime(format)
-    else
-      time_with_zone.strftime(format)
+      time_with_zone = time.in_time_zone(timezone)
+      if time_with_zone.dst? && !observes_dst
+        offset -= 120
+        timezone = ActiveSupport::TimeZone[offset/60]
+        2.hours.since(time.in_time_zone(timezone)).strftime(format)
+      else
+        time_with_zone.strftime(format)
+      end
+    end
+  end
+
     end
   end
 end
--- spec/ozone_spec.rb
+++ spec/ozone_spec.rb
@@ -1,52 +1,61 @@
 require 'spec_helper'
 
-describe Ozone do
-  it 'has a version number' do
-    expect(Ozone::VERSION).not_to be nil
-  end
+module Ozone
+  describe Formatter do
+    context 'given offset and observes daylight savings boolean' do
+      context 'time is not during daylight savings' do
+        it 'converts a utc time to time with time zone and makes no dst adustment' do
+          time_without_dst = ::Time.parse("2014-03-09 09:59:00 UTC")
+          adjusted_time = Formatter.call(
+            time: time_without_dst,
+            offset: -480,
+            observes_dst: true,
+          )
+          expect(adjusted_time).to eq("2014-03-09 01:59")
+        end
 
-  context 'given offset and observes daylight savings boolean' do
-    context 'time is not during daylight savings' do
-      it 'converts a utc time to time with time zone and makes no dst adustment' do
-        time_without_dst = Time.parse("2014-03-09 09:59:00 UTC")
-        adjusted_time = Ozone.call(
-          time: time_without_dst,
-          offset: -480,
-          observes_dst: true,
-        )
-        expect(adjusted_time).to eq("2014-03-09 01:59")
+        it 'converts a utc time to time with time zone and makes no dst adjustment' do
+          time_without_dst = ::Time.parse("2014-03-09 09:59:00 UTC")
+          adjusted_time = Formatter.call(
+            time: time_without_dst,
+            offset: -480,
+            observes_dst: false,
+          )
+          expect(adjusted_time).to eq("2014-03-09 01:59")
+        end
       end
 
-      it 'converts a utc time to time with time zone and makes no dst adjustment' do
-        time_without_dst = Time.parse("2014-03-09 09:59:00 UTC")
-        adjusted_time = Ozone.call(
-          time: time_without_dst,
-          offset: -480,
-          observes_dst: false,
-        )
-        expect(adjusted_time).to eq("2014-03-09 01:59")
+      context 'time is during daylight savings' do
+        it 'converts a utc time to time with time zone and makes no dst adustment' do
+          time_dst = ::Time.parse("2014-03-09 10:00:00 UTC")
+          adjusted_time = Formatter.call(
+            time: time_dst,
+            offset: -480,
+            observes_dst: true,
+          )
+          expect(adjusted_time).to eq("2014-03-09 03:00")
+        end
+
+        it 'converts a utc time to time with time zone and moves clocks back an hour' do
+          time_dst = ::Time.parse("2014-03-09 10:00:00 UTC")
+          adjusted_time = Formatter.call(
+            time: time_dst,
+            offset: -480,
+            observes_dst: false,
+          )
+          expect(adjusted_time).to eq("2014-03-09 02:00")
+        end
+      end
+    end
+  end
+
+      end
+
       end
     end
 
-    context 'time is during daylight savings' do
-      it 'converts a utc time to time with time zone and makes no dst adustment' do
-        time_dst = Time.parse("2014-03-09 10:00:00 UTC")
-        adjusted_time = Ozone.call(
-          time: time_dst,
-          offset: -480,
-          observes_dst: true,
-        )
-        expect(adjusted_time).to eq("2014-03-09 03:00")
       end
 
-      it 'converts a utc time to time with time zone and moves clocks back an hour' do
-        time_dst = Time.parse("2014-03-09 10:00:00 UTC")
-        adjusted_time = Ozone.call(
-          time: time_dst,
-          offset: -480,
-          observes_dst: false,
-        )
-        expect(adjusted_time).to eq("2014-03-09 02:00")
       end
     end
   end
