15244a70b87c3a0f6999f08491626696b629f34f
Hubert Huang <hubert77@gmail.com>
2015-07-22 14:22:43 -0700

    Create Ozone::Time class for comparing ozone times with ruby times

--- lib/ozone.rb
+++ lib/ozone.rb
@@ -19,6 +19,33 @@ module Ozone
     end
   end
 
+  class Time
+    def initialize(time:, offset:, observes_dst: true)
+      @offset = offset
+      @timezone = ActiveSupport::TimeZone[offset/60]
+      @observes_dst = observes_dst
+      @time = time
+    end
+
+    def before?(time_with_zone)
+      adjusted_time < time_with_zone
+    end
+
+    def after?(time_with_zone)
+      !before?(time_with_zone)
+    end
+
+    def strftime(format: '%Y-%m-%d %H:%M')
+      Formatter.call(time: @time, offset: @offset, observes_dst: @observes_dst, format: format)
+    end
+
+    private
+
+    def adjusted_time
+      time_with_zone = @time.in_time_zone(@timezone)
+      time_with_zone.dst? && !@observes_dst ?
+        1.hour.until(time_with_zone) :
+        time_with_zone
     end
   end
 end
--- spec/ozone_spec.rb
+++ spec/ozone_spec.rb
@@ -49,13 +49,103 @@ module Ozone
     end
   end
 
+  describe Time do
+    describe '#strftime' do
+      context 'time is not during daylight savings' do
+        let(:time_without_dst) { ::Time.parse("2014-03-09 09:59:00 UTC") }
+
+        it 'converts a utc time to time with time zone and makes no dst adustment' do
+          adjusted_time = Time.new(
+            time: time_without_dst,
+            offset: -480,
+            observes_dst: true,
+          )
+          expect(adjusted_time.strftime).to eq("2014-03-09 01:59")
+        end
+
+        it 'converts a utc time to time with time zone and makes no dst adjustment' do
+          adjusted_time = Time.new(
+            time: time_without_dst,
+            offset: -480,
+            observes_dst: true,
+          )
+          expect(adjusted_time.strftime).to eq("2014-03-09 01:59")
+        end
       end
 
+      context 'time is during daylight savings' do
+        let(:time_with_dst) { ::Time.parse("2014-03-09 10:00:00 UTC") }
+
+        it 'converts a utc time to time with time zone and makes no dst adustment' do
+          adjusted_time = Time.new(
+            time: time_with_dst,
+            offset: -480,
+            observes_dst: true,
+          )
+          expect(adjusted_time.strftime).to eq("2014-03-09 03:00")
+        end
+
+        it 'converts a utc time to time with time zone and moves clocks back an hour' do
+          adjusted_time = Time.new(
+            time: time_with_dst,
+            offset: -480,
+            observes_dst: false,
+          )
+          expect(adjusted_time.strftime).to eq("2014-03-09 02:00")
+        end
       end
     end
 
+    describe '#before' do
+      context  'time outside daylight savings' do
+        let(:time_without_dst) { ::Time.parse("2014-03-09 09:59:00 UTC") }
+
+        it 'returns true o time with time zone and makes no dst adustment' do
+          ozone_time = Time.new(
+            time: time_without_dst,
+            offset: -480,
+            observes_dst: true,
+          )
+          expect(ozone_time.before?(1.second.since(time_without_dst))).to be_truthy
+          expect(ozone_time.after?(1.second.since(time_without_dst))).to be_falsey
+          expect(ozone_time.before?(1.second.until(time_without_dst))).to be_falsey
+        end
+
+        it 'converts a utc time to time with time zone and makes no dst adjustment' do
+          ozone_time = Time.new(
+            time: time_without_dst,
+            offset: -480,
+            observes_dst: false,
+          )
+          expect(ozone_time.before?(1.second.since(time_without_dst))).to be_truthy
+          expect(ozone_time.after?(1.second.since(time_without_dst))).to be_falsey
+          expect(ozone_time.before?(1.second.until(time_without_dst))).to be_falsey
+        end
       end
 
+      context 'time is during daylight savings' do
+        let(:time_with_dst) { ::Time.parse("2014-03-09 10:00:00 UTC") }
+
+        it 'converts a utc time to time with time zone and makes no dst adustment' do
+          ozone_time = Time.new(
+            time: time_with_dst,
+            offset: -480,
+            observes_dst: true,
+          )
+          expect(ozone_time.before?(1.second.since(time_with_dst))).to be_truthy
+          expect(ozone_time.before?(1.second.until(time_with_dst))).to be_falsey
+        end
+
+        it 'converts a utc time to time with time zone and moves clocks back an hour' do
+          ozone_time = Time.new(
+            time: time_with_dst,
+            offset: -480,
+            observes_dst: false,
+          )
+
+          expect(ozone_time.before?(1.second.since(time_with_dst))).to be_truthy
+          expect(ozone_time.before?(1.second.until(time_with_dst))).to be_truthy
+        end
       end
     end
   end
